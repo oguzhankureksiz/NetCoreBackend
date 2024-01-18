eea609bce2a65dffa1d5850be268cf9ee1a0c99f
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-01-18 18:04:17 +0300

    Fluent Validation and ValidationAspect added.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,18 +23,11 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if(car.Description.Length >= 2 && car.DailyPrice > 0)
-            {
                 _carDal.Add(car);
                 return new SuccessResult(Messages.CarAdded);
-            }
-            else
-            {
-                return new ErrorResult(Messages.CarNameIsInvalid);
-            }
-            
         }
 
         public IResult Delete(Car car)
--- Business/ValidationRules/FluentValidation/CarValidator.cs
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -15,7 +15,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(c => c.Description).NotEmpty();
             RuleFor(c => c.Description).MinimumLength(2);
             RuleFor(c => c.DailyPrice).NotEmpty();
-            RuleFor(c => c.DailyPrice).LessThan(0);
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
         }
     }
 }
